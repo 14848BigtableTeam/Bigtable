cd82d650cedcc339551f7df18f25d1acd17c2be0
Chi Zhang <czhang5@andrew.cmu.edu>
2019-10-19 16:36:10 -0400

    add mem_search

--- starter/op_api.py
+++ starter/op_api.py
@@ -5,11 +5,16 @@ class MemTable:
         self.length = 0
         self.table = []
 
-    def insert(self, table_name, payload, wal_path):
+    def insert(self, table_name, payload, wal_path): 
         pass
 
     def retrieve(self, table_name, payload):
-        pass
+        row = payload["row"]
+        column_family = payload["column_family"]
+        column = payload["column"]
+        Memfind = find_row_index(self.table, row)
+        if self.table[Memfind]["row"] !=  
+
 
     def spill(self, ssindex_path, wal_path):
         pass
@@ -26,6 +31,29 @@ def find_row_index(table, row_key):
             right = mid - 1
     return left
 
+def mem_find_row_index(table, row_key, table_name):
+    left = 0
+    right = len(table) - 1
+    while left <= right:
+        mid = left + (right-left) // 2
+        if table[mid]['row'] < row_key:
+            left = mid + 1
+        else:
+            right = mid - 1
+    if left == len(table) or table[left]['row'] != row_key:
+        return left
+    else: 
+        tmpindex = left
+        while tmpindex < len(table) and table[tmpindex]['row'] == row_key and table[tmpindex]['table_name'] != table_name:
+            tmpindex += 1
+        if tmpindex < len(table) and table[tmpindex]['row'] == row_key and table[tmpindex]['table_name'] == table_name:
+            return tmpindex
+        tmpindex = left
+        while tmpindex >= 0 and table[tmpindex]['row'] == row_key and table[tmpindex]['table_name'] != table_name:
+            tmpindex -= 1
+        if tmpindex >= 0 and table[tmpindex]['row'] == row_key and table[tmpindex]['table_name'] != table_name:
+            return tmpindex
+        return left
 
 
 def insert_single_cell(payload, memtable, memindex):

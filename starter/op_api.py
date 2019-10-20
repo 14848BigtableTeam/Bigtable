4f624f418b7490b4c76d6c8373abf472eb7bb971
Can <canx@andrew.cmu.edu>
2019-10-20 00:32:35 -0700

    fix conflict

--- starter/op_api.py
+++ starter/op_api.py
@@ -23,7 +23,8 @@ class MemTable:
                 self.table[row_insert_index]['table_name'] != table_name:
             # size of memtable has reached to the maximum value
             if len(self.table) == self.max_entries:
-                self.spill(start = 0, mem_index=mem_index, ssindex_path=ssindex_path, wal_path=wal_path, metadata=metadata)
+                self.spill(start=0, mem_index=mem_index, ssindex_path=ssindex_path, wal_path=wal_path,
+                           metadata=metadata)
             # cannot find a specific row, insert a new one
             new_row = {
                 'row': row_key,
@@ -72,13 +73,12 @@ class MemTable:
             del retrieve_data[0: len(retrieve_data) - 5]
         return {"row": row, "data": retrieve_data}
 
-
     def retrieve_row(self, table_name, payload, metadata, mem_index):
         row_key = payload['row']
 
         # retrieve result in memtable
         memtable_res = [row_res for row_res in self.table if
-                        row_res['table_name'] == table_name and row_res['row'] == 'row_key']
+                        row_res['table_name'] == table_name and row_res['row'] == row_key]
         memtable_res = memtable_res[0] if len(memtable_res) else {}
         sstable_res = {}
         # retrieve result  in sstable in disk
@@ -102,11 +102,28 @@ class MemTable:
                 all_res_column = {metadata_column: {'data': []}}
                 all_res_column_family[metadata_column_family['column_family_key']]['columns'].append(all_res_column)
             all_res['column_families'].append(all_res_column_family)
-
-                
+            for all_res_column_family_dict in all_res['column_families']:
+                for all_res_column_family_key, all_res_columns_dict in all_res_column_family_dict.items():
+                    all_res_columns = all_res_columns_dict['columns']
+                    for all_res_column_dict in all_res_columns:
+                        for all_res_column_key, all_res_data_dict in all_res_column_dict.items():
+                            all_res_data = all_res_data_dict['data']
+                            if sstable_res != {}:
+                                for sstable_data in sstable_res['column_families'][all_res_column_family_key][
+                                    all_res_column_key]:
+                                    if len(all_res_data) == self.cell_data_max_num:
+                                        all_res_data.pop(0)
+                                    all_res_data.append(sstable_data)
+                            if memtable_res != {}:
+                                for memtable_data in memtable_res['column_families'][all_res_column_family_key][
+                                    all_res_column_key]:
+                                    if len(all_res_data) == self.cell_data_max_num:
+                                        all_res_data.pop(0)
+                                    all_res_data.append(memtable_data)
+            return all_res
 
     def spill(self, start, mem_index, ssindex_path, wal_path, metadata):
-        c_table = self.table[start :]
+        c_table = self.table[start:]
         row_table = classify(c_table, mem_index)
         wal_table = wal_classify(c_table)
         with open("./classify.json", "w") as f:
@@ -131,7 +148,8 @@ class MemTable:
                             if metadata[table_name]["row_num"][-1] == 1000:
                                 with open(subtable_path, 'w') as f:
                                     json.dump(subtable, f)
-                                last_file = metadata[table_name]["filenames"][-1][0 : len(metadata[table_name]["filenames"][-1]) - 5]
+                                last_file = metadata[table_name]["filenames"][-1][
+                                            0: len(metadata[table_name]["filenames"][-1]) - 5]
                                 last_file = last_file.split('_')
                                 filenum = str(int(last_file[-1]) + 1)
                                 last_file.pop()
@@ -156,7 +174,7 @@ class MemTable:
         with open(Global.get_metadata_path(), 'w') as f:
             json.dump(metadata, f)
         with open(ssindex_path, 'w') as f:
-            json.dump(mem_index, f) 
+            json.dump(mem_index, f)
         if start != 0:
             WALlist = []
             with open(wal_path, 'r') as f:
@@ -177,21 +195,23 @@ class MemTable:
             self.spill(num, mem_index, ssindex_path, wal_path, metadata)
         self.max_entries = num
 
-def classify (c_table, mem_index):
-        row_table = {}
-        for row in c_table:
-            row_key = row["row"]
-            table_name = row["table_name"]
-            column_family = row["column_families"]
-            if table_name not in row_table:
-                row_table[table_name] = {"Not":[]}
-            if row_key in mem_index and table_name in mem_index[row_key]:
-                if mem_index[row_key][table_name] not in row_table[table_name]:
-                    row_table[table_name][mem_index[row_key][table_name]] = []
-                row_table[table_name][mem_index[row_key][table_name]].append(row)
-            else:
-                row_table[table_name]["Not"].append(row)
-        return row_table
+
+def classify(c_table, mem_index):
+    row_table = {}
+    for row in c_table:
+        row_key = row["row"]
+        table_name = row["table_name"]
+        column_family = row["column_families"]
+        if table_name not in row_table:
+            row_table[table_name] = {"Not": []}
+        if row_key in mem_index and table_name in mem_index[row_key]:
+            if mem_index[row_key][table_name] not in row_table[table_name]:
+                row_table[table_name][mem_index[row_key][table_name]] = []
+            row_table[table_name][mem_index[row_key][table_name]].append(row)
+        else:
+            row_table[table_name]["Not"].append(row)
+    return row_table
+
 
 def wal_classify(c_table):
     wal_table = []
@@ -203,14 +223,15 @@ def wal_classify(c_table):
 
 
 def merge_row(subtable, row):
-        row_key = row["row"]
-        row_index = find_row_index(subtable, row_key)
-        for column_family in subtable[row_index]["column_families"]:
-            for column in subtable[row_index]["column_families"][column_family]:
-                subtable_list = subtable[row_index]["column_families"][column_family][column]
-                subtable_list = subtable + row["column_families"][column_family][column]
-                if len(subtable_list) > 5:
-                    del subtable_list[0 : len(subtable_list) - 5]
+    row_key = row["row"]
+    row_index = find_row_index(subtable, row_key)
+    for column_family in subtable[row_index]["column_families"]:
+        for column in subtable[row_index]["column_families"][column_family]:
+            subtable_list = subtable[row_index]["column_families"][column_family][column]
+            subtable_list = subtable + row["column_families"][column_family][column]
+            if len(subtable_list) > 5:
+                del subtable_list[0: len(subtable_list) - 5]
+
 
 def add_row(subtable, row):
     row.pop("table_name")
@@ -253,7 +274,3 @@ def mem_find_row_index(table, row_key, table_name):
         if tmpindex >= 0 and table[tmpindex]['row'] == row_key and table[tmpindex]['table_name'] != table_name:
             return tmpindex
         return left
-
-
-def retrieve_single_cell(payload, memtable, memindex):
-    pass
